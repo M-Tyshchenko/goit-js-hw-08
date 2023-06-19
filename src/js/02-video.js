a3d82f1c3b61f2e82d73a84024730c7add0bd55d
M-Tyshchenko <m.tish2406@gmail.com>
2023-06-19 23:52:07 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,11 +2,3 @@ import Vimeo from '@vimeo/player';
 
    const iframe = document.querySelector('iframe');
     const player = new Vimeo(iframe);
-
-    player.on('play', function() {
-        console.log('played the video!');
-    });
-
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });

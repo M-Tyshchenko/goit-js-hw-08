cde4fa64a26ff39379818cec76e96a89e75bbbec
M-Tyshchenko <m.tish2406@gmail.com>
2023-06-19 22:44:09 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,10 +1,12 @@
-import Player from '@vimeo/player';
+import Vimeo from '@vimeo/player';
 
-const player = new Player('handstick', {
-    id: 19231868,
-    width: 640
-});
+   const iframe = document.querySelector('iframe');
+    const player = new Vimeo(iframe);
 
-player.on('play', function() {
-    console.log('played the video!');
-});
+    player.on('play', function() {
+        console.log('played the video!');
+    });
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });

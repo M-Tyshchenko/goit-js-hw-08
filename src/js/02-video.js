b4bd9935502c29fae08bbd19dc28536c4038fcc3
M-Tyshchenko <m.tish2406@gmail.com>
2023-06-20 23:47:25 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,4 +1,20 @@
 import Vimeo from '@vimeo/player';
+import throttle from "lodash.throttle";
 
    const iframe = document.querySelector('iframe');
     const player = new Vimeo(iframe);
+
+const LOCALSTORAGE_KEY = "videoplayer-current-time";
+
+player.on('timeupdate', throttle(getCurrentTime, 1000));
+
+function getCurrentTime(event) {
+   localStorage.setItem(LOCALSTORAGE_KEY, event.seconds);
+}
+
+const currentTime = localStorage.getItem(LOCALSTORAGE_KEY);
+const currentTimeNumeric = Number(currentTime);
+
+if(currentTime) {
+   player.setCurrentTime(currentTimeNumeric);
+}
